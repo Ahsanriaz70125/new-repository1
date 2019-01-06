0a72b1722b6b86ed3d7656ab54100e1879c22c2f
frodomessi <ahad7358@gmail.com>
2019-01-06 11:42:18 +0500

    Initial Commit

---
+++ imagscroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imagscroll
+//
+//  Created by SGI-Mac7 on 06/01/2019.
+//  Copyright © 2019 Slash Global. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
