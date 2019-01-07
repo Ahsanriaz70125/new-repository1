b9a3b15cdcfca3a1cc7d398cc2ae4d4ec49d2e23
frodomessi <ahad7358@gmail.com>
2019-01-07 15:29:10 +0500

    imagescroll

---
+++ imagscroll/DataCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DataCollectionViewCell.swift
+//  imagscroll
+//
+//  Created by SGI-Mac7 on 06/01/2019.
+//  Copyright © 2019 Slash Global. All rights reserved.
+//
+
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+}
--- imagscroll/ViewController.swift
+++ imagscroll/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colltionView: UICollectionView!
+    var imgArry = [UIImage(named: "1"),
+                   UIImage(named: "2"),
+                   UIImage(named: "3"),
+                   UIImage(named: "4"),
+                   UIImage(named: "5"),]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgArry.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        as? DataCollectionViewCell
+        cell?.img.image = imgArry[indexPath.row]
+        return cell!
+    }
+}
+
+extension ViewController:UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = UIScreen.main.bounds
+        return CGSize(width: size.width, height: size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+}
+
 
